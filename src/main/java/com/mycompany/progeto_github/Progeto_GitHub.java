e0896ce66c3ef76db37ad05dfc253c9b7db2bfb4
Diogo Costa <a17464@d1-08-03.dped.local>
2023-02-06 16:12:09 +0000

    Criação do repositório local

--- src/main/java/com/mycompany/progeto_github/Progeto_GitHub.java
+++ src/main/java/com/mycompany/progeto_github/Progeto_GitHub.java
@@ -2,8 +2,9 @@
 package com.mycompany.progeto_github;
 public class Progeto_GitHub {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {        
+        System.out.println("Ola Mundinho!");
+        //Falta aqui um comentario...
 
 
     }
